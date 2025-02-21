b5b90e56345e5fbd09addf1366f303b16429ab2d
Garry <garry.joly@hotmail.fr>
2025-02-21 14:57:40 +0100

    add searchBar to filter items

---
+++ mon_dressing/View/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  mon_dressing
+//
+//  Created by garry joly on 26/02/2025.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
